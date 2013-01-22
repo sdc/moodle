16494f4c17fd82cdcf1f210bce266ef0598e75e4
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-01-22 14:44:56 +0000

    Twitter Search block.

---
+++ blocks/twitter_search/lang/en/block_twitter_search.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['pluginname'] = 'Twitter Search Block';
+$string['twitter_search'] = 'Twitter Search';
+$string['search_term'] = 'Search Term';
+$string['title_block'] = 'Visible block title';
+$string['numtweets'] = 'Number of tweets to show';
+$string['on_twitter'] = ' on Twitter';
+$string['not_found'] = 'We can\'t contact with Twitter, please come back later!';
+$string['show_names'] = 'Show real names';
+$string['show_usernames'] = 'Show usernames on Twitter';
+$string['show_images'] = 'Show user images';
+$string['show_update'] = 'Show update link';
+$string['update'] = 'Update';
---
+++ blocks/twitter_search/lang/es/block_twitter_search.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['pluginname'] = 'Bloque de b&uacute;squeda de Twitter';
+$string['twitter_search'] = 'B&uacute;squeda de Twitter';
+$string['search_term'] = 'T&eacute;rmino de b&uacute;squeda';
+$string['title_block'] = 'T&iacute;tulo del bloque';
+$string['numtweets'] = 'N&uacute;mero de tweets a mostrar';
+$string['on_twitter'] = ' en Twitter';
+$string['not_found'] = 'No podemos contactarnos con Twitter, vuelve m&aacute;s tarde';
+$string['show_names'] = 'Mostrar nombres reales';
+$string['show_usernames'] = 'Mostrar nombres de usuario de Twitter';
+$string['show_images'] = 'Mostrar im&aacute;genes de usuario';
+$string['show_update'] = 'Mostrar enlace de actualizar';
+$string['update'] = 'Actualizar';
