6aba0cc4aff4a1da3cb877e912e604df8d0adf7a
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-01-23 09:46:45 +0000

    Log in as... local plugin.

---
+++ local/loginas/lang/en/local_loginas.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings
+ *
+ * @package    local
+ * @subpackage loginas
+ * @copyright  2012 Itamar Tzadok
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['ajaxnext25'] = 'Next 25 >>';
+$string['ajaxoneuserfound'] = '1 user found';
+$string['ajaxprev25'] = '<< Prev 25';
+$string['ajaxxusersfound'] = '{$a} users found';
+$string['configcourseusers'] = 'Show link to a dialogue login-as list of course users in the course level.';
+$string['configloginasusernames'] = 'A comma separated list of usernames (e.g. johndoh,janeduh) of users to show in a login as menu in the settings navigation.';
+$string['configloginasusers'] = 'A comma separated list of user ids (e.g. 101,1212) of users to show in a login as menu in the settings navigation.';
+$string['courseusers'] = 'Course users';
+$string['errajaxsearch'] = 'Error when searching users';
+$string['loginasuser'] = 'Login as user';
+$string['loginasusers'] = 'Login as users by id';
+$string['loginasusernames'] = 'Login as users by username';
+$string['pluginname'] = 'Login as';
+$string['unknowajaxaction'] = 'Unknown action requested';
