7a9217e8d27ccb938b66f8d63dfb2a478c40c63a
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-01-23 10:39:04 +0000

    Realtime Quiz mod.

---
+++ mod/realtimequiz/version.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Code fragment to define the version of realtimequiz
+ * This fragment is called by moodle_needs_upgrading() and /admin/index.php
+ *
+ * @author: Davosmith
+ * @package realtimequiz
+ **/
+
+defined('MOODLE_INTERNAL') || die();
+
+$module->version   = 2013011000;  // The current module version (Date: YYYYMMDDXX)
+$module->requires  = 2010112400;  // Moodle 2.0 (or above)
+$module->cron      = 0;           // Period for cron to check this module (secs)
+$module->component = 'mod_realtimequiz';
+$module->maturity  = MATURITY_STABLE;
+$module->release   = '2.x (Build: 2013011000)';
