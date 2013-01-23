dbd88cad0bfcbe8f028587a0e5c6609a42f86c43
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-01-23 16:05:24 +0000

    Mediacapture repository.

---
+++ repository/mediacapture/recorders/red5recorder/record.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  2012 Ankit Gupta <mailtoankitgupta@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . '/lib.php');
+
+$client = new repository_mediacapture_red5recorder();
+$tmpdir = $client->temp_dir();
+
+$tmpfile = required_param('filepath', PARAM_RAW);
+$tmpname = required_param('filename', PARAM_TEXT);
+
+$tmpfile = urldecode($tmpfile);
+$path = "$tmpdir/$tmpname.flv";
+
+$fp = fopen($path, 'w');
+$ch = curl_init($tmpfile);
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_exec($ch);
+curl_close($ch);
+// Close file handler.
+fclose($fp);
+
+echo $path;
