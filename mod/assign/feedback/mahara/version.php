a2dc776c64055f823805f6638dca43d01ad19837
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-08-22 09:13:09 +0100

    Mahara local plugin, submission and feedback plugins.

---
+++ mod/assign/feedback/mahara/version.php
@@ -0,0 +1,9 @@
+<?php
+
+$plugin->version = 2013061200;
+$plugin->requires = 2012062500;
+$plugin->component = 'assignfeedback_mahara';
+
+$plugin->dependencies = array(
+    'local_mahara' => ANY_VERSION
+);
