a4663d163eed31fbfb108ee8429587ca70c02116
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-10-29 14:20:56 +0000

    Scheduler mod updates (2.5).

--- mod/scheduler/version.php
+++ mod/scheduler/version.php
@@ -5,21 +5,20 @@
  *
  * @package    mod
  * @subpackage scheduler
- * @copyright  2011 Henning Bostelmann and others (see README.txt)
+ * @copyright  2013 Henning Bostelmann and others (see README.txt)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * This is the 2.3 branch (MOODLE_23_STABLE) of the scheduler module.
- * It is intended for use with Moodle 2.3 and above.
+ * This is the Moodle 2.5 branch of the scheduler module.
  */
 
-$module->version  = 2012102803;       // The current module version (Date: YYYYMMDDXX)
-$module->release  = '2.3.1+';         // Human-friendly version name
-$module->requires = 2012062500;       // Requires Moodle 2.3
-$module->maturity = MATURITY_STABLE;  // Stable release
+$module->version  = 2013092701;       // The current module version (Date: YYYYMMDDXX)
+$module->release  = '2.5.0+';         // Human-friendly version name
+$module->requires = 2013051400;       // Requires Moodle 2.5
+$module->maturity = MATURITY_STABLE;  // Stable branch
 
 $module->cron     = 60;               // Period for cron to check this module (secs)
 
