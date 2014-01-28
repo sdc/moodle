jishnugopim/moodle-1

MDL-42928 JavaScript: Add LockScroll to all modal dialogues

lib/yui/build/moodle-core-notification-dialogue/moodle-core-notification-dialogue-debug.js
@@ -129,6 +129,10 @@ Y.extend(DIALOGUE, Y.Panel, {
         }
         this.set('COUNT', COUNT);
 
+        if (this.get('modal')) {
+            this.plug(Y.Moodle.core.LockScroll);
+        }
+
         // Workaround upstream YUI bug http://yuilibrary.com/projects/yui3/ticket/2532507
         // and allow setting of z-index in theme.
         bb = this.get('boundingBox');
@@ -334,7 +338,24 @@ Y.extend(DIALOGUE, Y.Panel, {
         } else if (content && content !== '') {
             content.focus();
         }
+
+        // Lock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.enableScrollLock();
+        }
+
         return result;
+    },
+
+    hide: function() {
+        Y.log("ide me");
+
+        // Unlock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.disableScrollLock();
+        }
+
+        return DIALOGUE.superclass.hide.call(this, arguments);
     },
     /**
      * Setup key delegation to keep tabbing within the open dialogue.
@@ -483,4 +504,4 @@ Y.extend(DIALOGUE, Y.Panel, {
 M.core.dialogue = DIALOGUE;
 
 
-}, '@VERSION@', {"requires": ["base", "node", "panel", "event-key", "dd-plugin"]});
+}, '@VERSION@', {"requires": ["base", "node", "panel", "event-key", "dd-plugin", "moodle-core-lockscroll"]});

lib/yui/build/moodle-core-notification-dialogue/moodle-core-notification-dialogue.js
@@ -129,6 +129,10 @@ Y.extend(DIALOGUE, Y.Panel, {
         }
         this.set('COUNT', COUNT);
 
+        if (this.get('modal')) {
+            this.plug(Y.Moodle.core.LockScroll);
+        }
+
         // Workaround upstream YUI bug http://yuilibrary.com/projects/yui3/ticket/2532507
         // and allow setting of z-index in theme.
         bb = this.get('boundingBox');
@@ -334,7 +338,23 @@ Y.extend(DIALOGUE, Y.Panel, {
         } else if (content && content !== '') {
             content.focus();
         }
+
+        // Lock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.enableScrollLock();
+        }
+
         return result;
+    },
+
+    hide: function() {
+
+        // Unlock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.disableScrollLock();
+        }
+
+        return DIALOGUE.superclass.hide.call(this, arguments);
     },
     /**
      * Setup key delegation to keep tabbing within the open dialogue.
@@ -483,4 +503,4 @@ Y.extend(DIALOGUE, Y.Panel, {
 M.core.dialogue = DIALOGUE;
 
 
-}, '@VERSION@', {"requires": ["base", "node", "panel", "event-key", "dd-plugin"]});
+}, '@VERSION@', {"requires": ["base", "node", "panel", "event-key", "dd-plugin", "moodle-core-lockscroll"]});

lib/yui/src/notification/js/dialogue.js
@@ -98,6 +98,10 @@ Y.extend(DIALOGUE, Y.Panel, {
         }
         this.set('COUNT', COUNT);
 
+        if (this.get('modal')) {
+            this.plug(Y.Moodle.core.LockScroll);
+        }
+
         // Workaround upstream YUI bug http://yuilibrary.com/projects/yui3/ticket/2532507
         // and allow setting of z-index in theme.
         bb = this.get('boundingBox');
@@ -303,7 +307,22 @@ Y.extend(DIALOGUE, Y.Panel, {
         } else if (content && content !== '') {
             content.focus();
         }
+
+        // Lock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.enableScrollLock();
+        }
+
         return result;
+    },
+
+    hide: function() {
+        // Unlock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.disableScrollLock();
+        }
+
+        return DIALOGUE.superclass.hide.call(this, arguments);
     },
     /**
      * Setup key delegation to keep tabbing within the open dialogue.

sdc/moodle

lib/yui/build/moodle-core-notification-dialogue/moodle-core-notification-dialogue-debug.js
@@ -118,6 +118,10 @@ Y.extend(DIALOGUE, Y.Panel, {
         }
         this.set('COUNT', COUNT);
 
+        if (this.get('modal')) {
+            this.plug(Y.M.core.LockScroll);
+        }
+
         // Workaround upstream YUI bug http://yuilibrary.com/projects/yui3/ticket/2532507
         // and allow setting of z-index in theme.
         bb = this.get('boundingBox');
@@ -317,6 +321,11 @@ Y.extend(DIALOGUE, Y.Panel, {
             header = this.headerNode,
             content = this.bodyNode;
 
+        // Lock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.enableScrollLock();
+        }
+
         result = DIALOGUE.superclass.show.call(this);
         if (header && header !== '') {
             header.focus();
@@ -324,6 +333,15 @@ Y.extend(DIALOGUE, Y.Panel, {
             content.focus();
         }
         return result;
+    },
+
+    hide: function() {
+        // Unlock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.disableScrollLock();
+        }
+
+        return DIALOGUE.superclass.hide.call(this, arguments);
     },
     /**
      * Setup key delegation to keep tabbing within the open dialogue.
@@ -550,4 +568,4 @@ Y.Base.modifyAttrs(DIALOGUE, {
 M.core.dialogue = DIALOGUE;
 
 
-}, '@VERSION@', {"requires": ["base", "node", "panel", "event-key", "dd-plugin"]});
+}, '@VERSION@', {"requires": ["base", "node", "panel", "event-key", "dd-plugin", "moodle-core-lockscroll"]});

lib/yui/build/moodle-core-notification-dialogue/moodle-core-notification-dialogue.js
@@ -118,6 +118,10 @@ Y.extend(DIALOGUE, Y.Panel, {
         }
         this.set('COUNT', COUNT);
 
+        if (this.get('modal')) {
+            this.plug(Y.M.core.LockScroll);
+        }
+
         // Workaround upstream YUI bug http://yuilibrary.com/projects/yui3/ticket/2532507
         // and allow setting of z-index in theme.
         bb = this.get('boundingBox');
@@ -317,6 +321,11 @@ Y.extend(DIALOGUE, Y.Panel, {
             header = this.headerNode,
             content = this.bodyNode;
 
+        // Lock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.enableScrollLock();
+        }
+
         result = DIALOGUE.superclass.show.call(this);
         if (header && header !== '') {
             header.focus();
@@ -324,6 +333,15 @@ Y.extend(DIALOGUE, Y.Panel, {
             content.focus();
         }
         return result;
+    },
+
+    hide: function() {
+        // Unlock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.disableScrollLock();
+        }
+
+        return DIALOGUE.superclass.hide.call(this, arguments);
     },
     /**
      * Setup key delegation to keep tabbing within the open dialogue.
@@ -548,4 +566,4 @@ Y.Base.modifyAttrs(DIALOGUE, {
 M.core.dialogue = DIALOGUE;
 
 
-}, '@VERSION@', {"requires": ["base", "node", "panel", "event-key", "dd-plugin"]});
+}, '@VERSION@', {"requires": ["base", "node", "panel", "event-key", "dd-plugin", "moodle-core-lockscroll"]});

lib/yui/src/notification/js/dialogue.js
@@ -87,6 +87,10 @@ Y.extend(DIALOGUE, Y.Panel, {
         }
         this.set('COUNT', COUNT);
 
+        if (this.get('modal')) {
+            this.plug(Y.M.core.LockScroll);
+        }
+
         // Workaround upstream YUI bug http://yuilibrary.com/projects/yui3/ticket/2532507
         // and allow setting of z-index in theme.
         bb = this.get('boundingBox');
@@ -286,6 +290,11 @@ Y.extend(DIALOGUE, Y.Panel, {
             header = this.headerNode,
             content = this.bodyNode;
 
+        // Lock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.enableScrollLock();
+        }
+
         result = DIALOGUE.superclass.show.call(this);
         if (header && header !== '') {
             header.focus();
@@ -293,6 +302,15 @@ Y.extend(DIALOGUE, Y.Panel, {
             content.focus();
         }
         return result;
+    },
+
+    hide: function() {
+        // Unlock scroll if the plugin is present.
+        if (this.lockScroll) {
+            this.lockScroll.disableScrollLock();
+        }
+
+        return DIALOGUE.superclass.hide.call(this, arguments);
     },
     /**
      * Setup key delegation to keep tabbing within the open dialogue.