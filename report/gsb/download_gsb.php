1575361f4d7cb0b00bff767d4bf443dbac710eb3
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-02-21 09:48:42 +0000

    Upgraded GSB report.

--- report/gsb/download_gsb.php
+++ report/gsb/download_gsb.php
@@ -25,7 +25,7 @@
  */
 
 require_once(dirname(__FILE__) . '/../../config.php');
-require_once (dirname(__FILE__) . '/../../lib/adminlib.php');
+require_once(dirname(__FILE__) . '/../../lib/adminlib.php');
 
 defined('MOODLE_INTERNAL') || die;
 
@@ -36,48 +36,51 @@ $context = context_system::instance();
 require_capability('report/gsb:viewmygsbreport', $context);
 
 
-function objectToArray($select) {
-		if (is_object($select)) {
-			// Gets the properties of the given object
-			// with get_object_vars function
-			$select = get_object_vars($select);
-		}
- 
-		if (is_array($select)) {
-			/*
-			* Return array converted to object
-			* Using __FUNCTION__ (Magic constant)
-			* for recursive call
-			*/
-			return array_map(__FUNCTION__, $select);
-		}
-		else {
-			// Return array
-			return $select;
-		}
-	}
-	
-$sql = "SELECT bgc.id, cc.name as category, c.fullname as coursename, c.shortname, bgc.linksnum AS resources, bgc.assignmentnum,
+function objectToArray($select)
+{
+    if (is_object($select)) {
+        // Gets the properties of the given object
+        // with get_object_vars function
+        $select = get_object_vars($select);
+    }
+    
+    if (is_array($select)) {
+        /*
+         * Return array converted to object
+         * Using __FUNCTION__ (Magic constant)
+         * for recursive call
+         */
+        return array_map(__FUNCTION__, $select);
+    } else {
+        // Return array
+        return $select;
+    }
+}
+
+$sql    = "SELECT bgc.id, cc.name as category, c.fullname as coursename, c.shortname, bgc.linksnum AS resources, bgc.assignmentnum,
 bgc.feedbacknum, bgc.questnum, bgc.quiznum, bgc.interactnum as interactive_learning_objects, bgc.embednum as embedded_videos,
 bgc.booknum, bgc.databasenum, bgc.workshopnum, bgc.choicenum, bgc.glossarynum,
-bgc.wikinum, bgc.chatnum, bgc.forumnum, bgc.foldersnum, bgc.urlsnum, bgc.gsb
+bgc.wikinum, bgc.chatnum, bgc.forumnum, bgc.foldersnum, bgc.urlsnum, bgc.headingsnum, bgc.gsb
 FROM {block_gsb} bgc
 JOIN {course} c ON bgc.ids = c.id
 JOIN {course_categories} cc ON c.category = cc.id
 ORDER by name ASC;";
 $select = $DB->get_records_sql($sql);
 
-$array = objectToArray($select);	
+$array = objectToArray($select);
 
 
-header("Content-type: text//octet-stream"); 
-header("Content-Disposition: attachment; filename=gsb_report.xls"); 
-$flag = false; 
+header("Content-type: text//octet-stream");
+header("Content-Disposition: attachment; filename=gsb_report.xls");
+$flag = false;
 
-foreach($array as $row) { if(!$flag) {
- // display field/column names as first row 
- echo implode("\t", array_keys($row)) . "\r\n"; $flag = true; 
- 
- }
- echo implode("\t", array_values($row)) . "\r\n"; } exit;
-
+foreach ($array as $row) {
+    if (!$flag) {
+        // display field/column names as first row 
+        echo implode("\t", array_keys($row)) . "\r\n";
+        $flag = true;
+        
+    }
+    echo implode("\t", array_values($row)) . "\r\n";
+}
+exit;
