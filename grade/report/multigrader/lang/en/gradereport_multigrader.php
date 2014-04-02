105eed961428d33404e100ca2d8f2f39fc6dfc49
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-04-02 14:20:09 +0100

    New 'multigrader' grade report.

---
+++ grade/report/multigrader/lang/en/gradereport_multigrader.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of the Multi Course Grader report for Moodle by Barry Oosthuizen http://elearningstudio.co.uk
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradereport_grader', language 'en'
+ *
+ * @package   gradereport_multigrader
+ * @copyright 2012 onwards Barry Oosthuizen http://elearningstudio.co.uk
+ * @author    Barry Oosthuizen
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['ajaxclicktoclose'] = 'Click this box to remove it';
+$string['ajaxerror'] = 'Error';
+$string['ajaxfailedupdate'] = 'Unable to update [1] for [2]';
+$string['ajaxfieldchanged'] = 'The field you are currently editing has changed, would you like to use the updated value?';
+$string['ajaxchoosescale'] = 'Choose';
+$string['multigrader:manage'] = 'Manage the multi grader report';
+$string['multigrader:view'] = 'View the multi grader report';
+$string['pluginname'] = 'Multi Grader report';
+$string['preferences'] = 'Multi Grader report preferences';
+$string['gradereport_multigrader'] = 'Multi Grader report';
