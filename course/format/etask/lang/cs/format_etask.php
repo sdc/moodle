fd2699b15cdfc383a3f4609d3cee3f5af585f2bc
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-04-02 14:34:13 +0100

    New 'etask' course format.

---
+++ course/format/etask/lang/cs/format_etask.php
@@ -0,0 +1,44 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_etask', language 'cs', branch 'MOODLE_26_STABLE'
+ *
+ * @package   format_etask
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['currentsection'] = 'Aktuální téma';
+$string['hidefromothers'] = 'Skrýt téma';
+$string['page-course-view-topics'] = 'Hlavní stránka libovolného kurzu v eTask formátu';
+$string['page-course-view-topics-x'] = 'Jakákoliv stránka kurzu s eTask uspořádáním';
+$string['pluginname'] = 'eTask uspořádání';
+$string['section0name'] = 'Úvod';
+$string['sectionname'] = 'Téma';
+$string['showfromothers'] = 'Zobrazit téma';
+
+$string['nodata'] = 'Pro modul eTask nejsou k dispozici žádná data.';
+$string['gradesettings'] = 'Nastavení známek';
+$string['legend'] = 'Legenda';
+$string['submitted'] = 'Odevzdáno';
+$string['passed'] = 'Splněno';
+$string['failed'] = 'Nesplněno';
+$string['notset'] = 'Nenastaveno';
+$string['save'] = 'Uložit';
---
+++ course/format/etask/lang/en/format_etask.php
@@ -0,0 +1,42 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_etask', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_etask
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['currentsection'] = 'This topic';
+$string['sectionname'] = 'Topic';
+$string['pluginname'] = 'eTask format';
+$string['section0name'] = 'General';
+$string['page-course-view-topics'] = 'Any course main page in eTask format';
+$string['page-course-view-topics-x'] = 'Any course page in eTask format';
+$string['hidefromothers'] = 'Hide topic';
+$string['showfromothers'] = 'Show topic';
+
+$string['nodata'] = 'There are no data for eTask module.';
+$string['gradesettings'] = 'Grade settings';
+$string['legend'] = 'Legend';
+$string['submitted'] = 'Submitted';
+$string['passed'] = 'Passed';
+$string['failed'] = 'Failed';
+$string['notset'] = 'Not set';
+$string['save'] = 'Save';
