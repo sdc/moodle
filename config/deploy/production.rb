8176e75dad480adfd9743700b58d65ff4f46110b
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-07-24 09:16:32 +0100

    Capistrano details.

---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+role :web, %w{deploy@moodle.southdevon.ac.uk deploy@appmoodle.southdevon.ac.uk}
