437b991cb2f5397f89e16a506732550300e2f1e7
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-08-27 14:17:10 +0100

    Updated Archaius theme.

--- theme/archaius/README.md
+++ theme/archaius/README.md
@@ -1,51 +1,128 @@
-Archaius
----------
+Archaius Theme
+==============
+
+What is Archaius
+----------------
 
 Archaius is a moodle's theme with several options to customize your Moodle:
 
- * Changing logo.
+ * Upload logo.
+ * Upload mobile logo.
  * Footnote.
  * Background color.
  * Header and footer color.
  * Tabs color (to be used with blocks titles).
- * Color for the current block tab.
+ * Color for the current block tab (block titles).
  * Color of the current item in custom menu.
  * Activation of the collapsible topics effect.
  * Activation of the hide/show blocks effect.
- * Activation frontpage responsive slideshow.
+ * Activation frontpage slideshow.
  * Custom CSS.
  * Custom Javascript in footer.
+ * Color of custom menu.
+ * Set timeout of the slider.
+ * Set height of the front page slider (pixels).
 
-Archaius uses jQuery as a javascript framework and it is using modernizr and responsive slides as well. it is using LESS to create CSS stylesheets, if you want to modify ir you should use it. For more information about 
+ @copyright  2013 onwards Daniel Munera Sanchez
 
-[LESS](http://lesscss.org/)
+Getting Starting
+----------------
 
-@copyright  2013 Daniel Munera Sanchez
+Archaius uses jQuery as a javascript framework and several jQuery plugins as well:
+
+* A modified version of [responsiveslides](https://github.com/dmuneras/ResponsiveSlides.js)
+* VelocityJS
+* Waypoints
+* Modernizr 
+
+I have been using [LESS](http://lesscss.org/) to create CSS stylesheets.
+
+
+If you want to modify the stylesheets, or jquery plugins, you should use Grunt. I configured a Gruntfile.js, so, you only need to install it and use it. Take a look of this
+website if you have any problem installing Grunt => [Getting Starting](http://gruntjs.com/getting-started)
+
+After read the Grunt getting starting, you only have to run the Grunt command in the root
+directory of archaius.
+
+```javascript
+
+grunt
+
+```
+After this any change you do in jQuery plugins or LESS source will be reflected in your CSS
+and Javascript.
+
+
 
 Improvements history:
-------------
-2.06.2014
+---------------------
 
-Improvements on JS, changing blocks effect.
+11.08.2014
+----------
+
+* Loading jquery plugins only when they are need.
+* new option to deactive accordion blocks.
+* new option to set up slideshow height using a number (pixels without the 'px')
+
+
+08.08.2014
+----------
+
+* Working on PHP improvements
+* new option to change color of custommenu
+* page header layout change
+* rewriting lib.php
+
+07.08.2014
+----------
+
+* Adding clear cache until theme settings update
+* Grunt added
+* Preview settings page is now working too.
+* Upload logo is possible in setting page
+* New setting : mobile logo
+* Velocity added to custom_blocks effect 
+
+06.07.2014
+----------
+
+* Adding options to manipulate Slideshow.
+
+2.06.2014
+---------
+
+* Improvements on JS, changing blocks effect.
 
 20.06.2013
-Better shape of block in the main content.
-New option in the setting page to activate and desactivate the hide and show blocks effect.
-Link inside main container are now blue to make them easier to find.
+----------
+
+* Better shape of block in the main content.
+* New option in the setting page to activate and desactivate the hide and show blocks effect.
+* Link inside main container are now blue to make them easier to find.
 
 09.09.2013
-New front page with a carousel of information that could be edited by admin users
+----------
+
+* New front page with a carousel of information that could be edited by admin users
 
 21.09.2013
-The creation of tabs for course topics was fixed. Dependencies between tabs and summaries have been erased (reported issue)
+-----------
+
+* The creation of tabs for course topics was fixed. Dependencies between tabs and summaries have been erased (reported issue)
 
 14.10.2013
-Better slideshow: possibility to activate or deactivate it, side-center-[pre|post] added.
+----------
+
+* Better slideshow: possibility to activate or deactivate it, side-center-[pre|post] added.
 
 
 05.12.2013
-Working on moodle 26 version and general improvements related with javascript.
+----------
+
+* Working on moodle 26 version and general improvements related with javascript.
 
 22.12.2013
-Writing LESS and reviewing CSS and Javascript as well as HTML5 fallback for IE.
+----------
+
+* Writing LESS and reviewing CSS and Javascript as well as HTML5 fallback for IE.
 Going responsive without Bootstrap.
--- theme/archaius/config.php
+++ theme/archaius/config.php
@@ -1,7 +1,6 @@
 <?php
 
-/*  
-
+/*
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -11,25 +10,40 @@ This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
-This plugin is part of Archaius theme.
-@copyright  2013 Daniel Munera Sanchez
-
 */
 
-////////////////////////////////////////////////////////////////////////////////
-/// This file contains a few configuration variables that control
-/// how Moodle uses this theme.
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * Archaius config
+ *
+ * @package theme_archaius
+ * @copyright 2013 onwards Daniel Munera
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/*
+ This file contains a few configuration variables that control
+ how Moodle uses this theme.
+-----------------------------------------------------------------------------*/
 
 $THEME->name = 'archaius';
 
+$allRegions = 
+    array(
+        'side-pre',
+        'side-post',
+        'side-center-pre',
+        'side-center-post',
+        'footer-left',
+        'footer-center',
+        'footer-right'
+    );
+
 $THEME->sheets = array(
-    'base_less',
-    'archaius_less',
-    'archaius_responsive_less',
-    'course_less',
-    'home_less',
+    'base',
+    'archaius',
+    'archaius_responsive',
+    'course',
+    'home',
     'boilerplate',
     'custom'
 );
@@ -45,61 +59,67 @@ $THEME->parents_exclude_sheets = array(
 $THEME->editor_sheets = array('editor');
 
 $THEME->layouts = array(
-    // Most pages - if we encounter an unknown or a missing page type, this one is used.
+    // Most pages - if we encounter an unknown or 
+    //a missing page type, this one is used.
     'base' => array(
         'file' => 'general.php',
-        'regions' => array()
+        'regions' => $allRegions,
+        'defaultregion' => 'side-pre'
     ),
     'standard' => array(
         'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post'),
+        'regions' => array(
+            'side-pre', 
+            'side-post'
+        ),
         'defaultregion' => 'side-post',
         'options' => array('langmenu' => true)
     ),
     // Course page
     'course' => array(
         'file' => 'general.php',
-        'regions' => array('side-pre','side-post',
-            'side-center-pre','side-center-post'),
+        'regions' => $allRegions,
         'defaultregion' => 'side-pre', 
         'options' => array('langmenu' => true, 'nonavbar' => false)
     ),
     // Course page
     'coursecategory' => array(
         'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post'),
+        'regions' => $allRegions,
         'defaultregion' => 'side-post',
         'options' => array('langmenu' => true)
     ),
     'incourse' => array(
         'file' => 'general.php',
-        'regions' => array('side-pre'),
+        'regions' => $allRegions,
         'defaultregion' => 'side-pre',
         'options' => array('langmenu' => true)
     ),
     'frontpage' => array(
         'file' => 'frontpage.php',
-        'regions' => array('side-pre', 'side-post',
-            'side-center-pre','side-center-post'),
+        'regions' => $allRegions,
         'defaultregion' => 'side-pre',
-        'options' => array('langmenu' => true, 'nonavbar' => true, 'nosubtitle' => true)
+        'options' => array(
+            'langmenu' => true, 
+            'nonavbar' => true, 
+            'nosubtitle' => true
+        )
     ),
     'admin' => array(
         'file' => 'general.php',
-        'regions' => array('side-pre'),
+        'regions' => $allRegions ,
         'defaultregion' => 'side-pre',
         'options' => array('langmenu' => true)
     ),
     'mydashboard' => array(
         'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post',
-            'side-center-pre','side-center-post'),
+        'regions' => $allRegions,
         'defaultregion' => 'side-post',
         'options' => array('langmenu' => true)
     ),
     'mypublic' => array(
         'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post'),
+        'regions' => $allRegions,
         'defaultregion' => 'side-post',
         'options' => array('langmenu' => true)
     ),
@@ -111,7 +131,11 @@ $THEME->layouts = array(
     'popup' => array(
         'file' => 'general.php',
         'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>true, 'noblocks'=>true),
+        'options' => array(
+            'nofooter'=>true, 
+            'nonavbar'=>true, 
+            'noblocks'=>true
+        ),
     ),
     // No blocks and minimal footer - used for legacy frame layouts only!
     'frametop' => array(
@@ -123,22 +147,36 @@ $THEME->layouts = array(
     'embedded' => array(
         'file' => 'general.php',
         'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>true, 'noblocks'=>true),
+        'options' => array(
+            'nofooter'=>true, 
+            'nonavbar'=>true, 
+            'noblocks'=>true
+        ),
     ),
     
-    // Used during upgrade and install, and for the 'This site is undergoing maintenance' message.
-    // This must not have any blocks, and it is good idea if it does not have links to
-    // other places - for example there should not be a home link in the footer...
+    // Used during upgrade and install, and for the 'This site is undergoing 
+    //maintenance' message.
+    // This must not have any blocks, and it is good idea if it does not 
+    //have links to other places - for example there should not be a home 
+    //link in the footer...
     'maintenance' => array(
         'file' => 'general.php',
         'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>true, 'noblocks'=>true),
+        'options' => array(
+            'nofooter'=>true, 
+            'nonavbar'=>true, 
+            'noblocks'=>true
+        ),
     ),
     // Should display the content and basic headers only.
     'print' => array(
         'file' => 'general.php',
         'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>false, 'noblocks'=>true),
+        'options' => array(
+            'nofooter'=>true, 
+            'nonavbar'=>false, 
+            'noblocks'=>true
+        ),
     ),
     'report' => array(
         'file' => 'report.php',
@@ -150,16 +188,11 @@ $THEME->layouts = array(
 
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 
-
 $THEME->javascripts_footer = array(
-    'jquery-1.11.0.min',
-    'modernizr',
-    'archaius_custom_blocks-1.1.1.min',
-    'responsiveslides.min',
+    'modernizr-2.7.1',
     'archaius'
 );
 
 $THEME->enable_dock = false;
-
 $THEME->editor_sheets = array('editor');
-$THEME->csspostprocess = 'archaius_process_css';
+$THEME->csspostprocess = 'theme_archaius_process_css';
