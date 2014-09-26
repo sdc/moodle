0536ff5f949231e728373e56122e6d02f5b573f5
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-09-26 10:17:57 +0100

    Updated Configurable Reports block.

--- blocks/configurable_reports/reports/sql/report.class.php
+++ blocks/configurable_reports/reports/sql/report.class.php
@@ -22,7 +22,7 @@
   * @date: 2009
   */
 
-define('REPORT_CUSTOMSQL_MAX_RECORDS', 5000);
+defined('REPORT_CUSTOMSQL_MAX_RECORDS') || define('REPORT_CUSTOMSQL_MAX_RECORDS', 5000);
 
 class report_sql extends report_base {
 
@@ -63,33 +63,10 @@ class report_sql extends report_base {
 
 		$sql = preg_replace('/\bprefix_(?=\w+)/i', $CFG->prefix, $sql);
 
-        // Use a custom $DB (and not current system's $DB)
-        // todo: major security issue
-        $remotedbhost = get_config('block_configurable_reports','dbhost');
-        if (empty($remotedbhost)) {
-            $remotedbhost = $CFG->dbhost;
-        }
-        $remotedbname = get_config('block_configurable_reports','dbname');
-        if (empty($remotedbname)) {
-            $remotedbname = $CFG->dbname;
-        }
-        $remotedbuser = get_config('block_configurable_reports','dbuser');
-        if (empty($remotedbuser)) {
-            $remotedbuser = $CFG->dbuser;
-        }
-        $remotedbpass = get_config('block_configurable_reports','dbpass');
-        if (empty($remotedbpass)) {
-            $remotedbpass = $CFG->dbpass;
-        }
-
         $reportlimit = get_config('block_configurable_reports','reportlimit');
         if (empty($reportlimit) or $reportlimit == '0') {
                 $reportlimit = REPORT_CUSTOMSQL_MAX_RECORDS;
         }
-
-        $db_class = get_class($DB);
-        $remotedb = new $db_class();
-        $remotedb->connect($remotedbhost, $remotedbuser, $remotedbpass, $remotedbname, $CFG->prefix);
 
         $starttime = microtime(true);
 
