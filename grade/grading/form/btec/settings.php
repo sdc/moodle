45131dff58050c5e25cb4f204558ff2f963ac067
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-09-26 10:28:03 +0100

    Updated BETA version of the BTEC grading form plugin.

--- grade/grading/form/btec/settings.php
+++ grade/grading/form/btec/settings.php
@@ -15,17 +15,18 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Contains the helper class for the select missing words question type tests.
- *
- * @package    qtype
- * @copyright  2012 Marcus Green
+ * @package    gradingform_btec
+ * @copyright  2013 Marcus Green
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-defined('MOODLE_INTERNAL') || die();
 
-class moodlequickform_bteceditor_helper {
+defined('MOODLE_INTERNAL') || die;
+/* sadly this file entirely ignored by moodle
+If it ever gets read it should also include counts
+ * of M and D criteria etc etc
+ **/
 
-    public function get_btec_editor() {
-        return new moodlequickform_bteceditor('testbteceditor', 'elementlabel', null);
-    }
-}
+$settings->add(new admin_setting_configtext(
+'gradingform_btec/passcount',
+        get_string('countofpasscriteria', 'gradingform_btec'), 6
+        ));
