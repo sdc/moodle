75e8539a05835e1a793e6f03caf27e9154f6a385
Paul Vaughan <paulieboo@gmail.com>
2015-02-06 12:57:04 +0000

    Updated Archaius theme.

--- theme/archaius/version.php
+++ theme/archaius/version.php
@@ -1,18 +1,22 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/*
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+This plugin is part of Archaius theme, if you use it outside the theme
+you should create your own styles. You can use archaius stylesheet as
+a example.
+@copyright  2014 onwards Daniel Munera Sanchez
+
+*/
 
 /**
  * Theme version info
@@ -25,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 // The current module version (Date: YYYYMMDDXX)
-$plugin->version   = 2014100300; 
+$plugin->version   = 2015011900; 
 // Requires this Moodle version
 $plugin->requires  = 2013040500; 
 // Full name of the plugin (used for diagnostics)
