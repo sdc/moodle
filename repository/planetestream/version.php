379043103ca6a798ba02fea85e02eb28735ae835
Paul Vaughan <paulieboo@gmail.com>
2015-02-06 12:55:05 +0000

    Updated Planet eStream repository.

--- repository/planetestream/version.php
+++ repository/planetestream/version.php
@@ -1,33 +1,19 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Planet eStream v5 Repository Plugin
+ * Planet eStream Repository Plugin
  *
- * @since       2.0
- * @package     repository_planetestream
- * @copyright   2013 Planet eStream
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @since 2.0
+ * @package    repository_planetestream
+ * @copyright  Planet Enterprises Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2014021700;
-$plugin->requires   = 2012062500;
-$plugin->component  = 'repository_planetestream';
-$plugin->maturity   = MATURITY_STABLE;
-$plugin->release    = '5.55';
+$plugin->version = 2014101300;
+$plugin->requires = 2012062500;
+$plugin->component = 'repository_planetestream';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '5.62';
