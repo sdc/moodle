0fef4ee3e899cbc70d5d54a3951584fd27177826
Paul Vaughan <paulieboo@gmail.com>
2015-02-06 08:55:19 +0000

    Removal of Sharing Cart block.

--- blocks/sharing_cart/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die;
-
-$plugin->version   = 2014091200;
-$plugin->requires  = 2012062501.00; // Moodle 2.3.1
-$plugin->component = 'block_sharing_cart';
-$plugin->release   = '2.6, release 1 patch 6';
-$plugin->maturity  = MATURITY_STABLE;
