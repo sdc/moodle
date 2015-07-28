ebe63974e337b432ea96b57427ed9204e7df4d5c
Paul Vaughan <paulieboo@gmail.com>
2015-07-28 10:27:43 +0100

    Updated Planet eStream plugin.

--- repository/planetestream/lang/en/repository_planetestream.php
+++ repository/planetestream/lang/en/repository_planetestream.php
@@ -1,7 +1,20 @@
 <?php
-
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Planet eStream v5 Repository Plugin
+ * Planet eStream Repository Plugin
  *
  * @since 2.0
  * @package    repository_planetestream
@@ -9,28 +22,26 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
  */
-
 /**
  * Strings for component 'repository_planetestream', language 'en', branch 'MOODLE_20_STABLE'
  */
-
-$string['pluginname']='Planet eStream Search';
-$string['comment']='Part of the Planet eStream solution';
-$string['search']='Search';
-$string['planetestream:view']='Use planetestream in file picker';
-$string['configplugin']='planetestream repository type configuration';
-$string['sort_orderby']='Order By';
-$string['sort_orderby_date']='Date Added';
-$string['sort_orderby_rating']='Rating';
-$string['sort_orderby_relevance']='Relevance';
-$string['sort_orderby_popularity']='Popularity';
-$string['sort_includechapters']='Include Chapters';
-$string['addedon']='Added on';
-$string['addedby']='by';
-$string['show']='Show';
-$string['show_all']='All Media Types';
-$string['show_video']='Videos';
-$string['show_playlist']='Playlists';
-$string['show_photoset']='Photosets';
-$string['settingsurl']='Planet eStream URL';
-$string['settingsurl_text']='Please enter the URL to your Planet eStream server.<br />e.g. http://videoserver.mysite.org<br />or https://videoserver.mysite.org<span>';
+$string['pluginname'] = 'Planet eStream Search';
+$string['comment'] = 'Part of the Planet eStream solution';
+$string['search'] = 'Search';
+$string['planetestream:view'] = 'Use planetestream in file picker';
+$string['configplugin'] = 'planetestream repository type configuration';
+$string['sort_orderby'] = 'Order By';
+$string['sort_orderby_date'] = 'Date Added';
+$string['sort_orderby_rating'] = 'Rating';
+$string['sort_orderby_relevance'] = 'Relevance';
+$string['sort_orderby_popularity'] = 'Popularity';
+$string['sort_includechapters'] = 'Include Chapters';
+$string['addedon'] = 'Added on';
+$string['addedby'] = 'by';
+$string['show'] = 'Show';
+$string['show_all'] = 'All Media Types';
+$string['show_video'] = 'Videos';
+$string['show_playlist'] = 'Playlists';
+$string['show_photoset'] = 'Photosets';
+$string['settingsurl'] = 'Planet eStream URL';
+$string['settingsurl_text'] = 'Please enter the URL to your Planet eStream server.<br />e.g. http://videoserver.mysite.org<br />or https://videoserver.mysite.org<span>';
