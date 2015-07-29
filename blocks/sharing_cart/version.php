35a49ad7b9fca0af7fa26a5a2e63e04d3ba0cf70
Paul Vaughan <paulieboo@gmail.com>
2015-07-29 12:48:56 +0100

    Updated Sharing Cart block.

---
+++ blocks/sharing_cart/version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2015061600;
+$plugin->requires  = 2012062501.00; // Moodle 2.3.1
+$plugin->component = 'block_sharing_cart';
+$plugin->release   = '2.9, release 1';
+$plugin->maturity  = MATURITY_STABLE;
