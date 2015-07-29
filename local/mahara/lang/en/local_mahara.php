c97bba432503ed0ebbf4f2d8ce1f08eaeff34f54
Paul Vaughan <paulieboo@gmail.com>
2015-07-29 11:15:50 +0100

    Removed Mahara local plugin.

--- local/mahara/lang/en/local_mahara.php
+++
@@ -1,4 +0,0 @@
-<?php
-$string['pluginname'] = 'Extra Mahara MNet Functions';
-$string['mahara_name'] = 'Extra Mahara MNet Module';
-$string['mahara_description'] = 'Mahara functions used in third-party Moodle plugins.<br />Publishing this service on a Moodle site has no effect. Subscribe to this service if you want to be able to use the Mahara plugins with {$a}.<br />';
