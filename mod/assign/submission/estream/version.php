11088e15abb54851c6b8890d723b2ce8fd248fb8
Paul Vaughan <paulieboo@gmail.com>
2015-10-09 11:44:31 +0100

    Updated Planet eStream Assign Submission plugin.

--- mod/assign/submission/estream/version.php
+++ mod/assign/submission/estream/version.php
@@ -23,8 +23,8 @@
  *
  */
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2015082500;
+$plugin->version = 2015100200;
 $plugin->requires = 2012062500;
 $plugin->component = 'assignsubmission_estream';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '6.00';
+$plugin->release = '6.02';
