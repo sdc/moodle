e820e2c09690b06b499571b3ef7bec59652af755
Paul Vaughan <p.vaughan@intrallect.com>
2016-07-08 10:23:49 +0100

    SDC-2 Updated Mass Action block to 1.2.4 (2016052401).

--- blocks/massaction/block_massaction.php
+++ blocks/massaction/block_massaction.php
@@ -15,12 +15,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package    blocks
- * @subpackage massaction
+ * @package    block_massaction
  * @copyright  2013 University of Minnesota
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 
 class block_massaction extends block_base {
 
@@ -31,7 +29,6 @@ class block_massaction extends block_base {
         $this->title = get_string('blocktitle', 'block_massaction');
     }
 
-
     /**
      * @see block_base::applicable_formats()
      */
@@ -39,14 +36,12 @@ class block_massaction extends block_base {
 	    return array('course-view' => true, 'mod' => false, 'tag' => false);
     }
 
-
     /**
      * no need to have multiple blocks to perform the same functionality
      */
     function instance_allow_multiple() {
         return false;
     }
-
 
     /**
      * @see block_base::get_content()
@@ -88,28 +83,32 @@ class block_massaction extends block_base {
             	'allitems'              => get_string('allitems', 'block_massaction'),
             	'deselectall'           => get_string('deselectall', 'block_massaction'),
                 'withselected'	        => get_string('withselected', 'block_massaction'),
-                'action_movetosection'	=> get_string('action_movetosection', 'block_massaction')
+                'action_movetosection'	=> get_string('action_movetosection', 'block_massaction'),
+                'action_duptosection'	=> get_string('action_duptosection', 'block_massaction')
             );
 
+            $jsdisabled = get_string('jsdisabled', 'block_massaction');
             $this->content->text  = <<< EOB
-<!--Select item one-by-one or:<br/-->
-<a id="mod-massaction-control-selectall" href="javascript:void(0);">{$str['selectall']}</a><br/>
-<select id="mod-massaction-control-section-list-select">
-	<option value="all">{$str['allitems']}</option>
-</select>
-<a id="mod-massaction-control-deselectall" href="javascript:void(0);">{$str['deselectall']}</a><br/><br/>
+<div class="block_massaction_jsdisabled">{$jsdisabled}</div>
+<div class="block_massaction_jsenabled hidden">
+    <a id="mod-massaction-control-selectall" href="javascript:void(0);">{$str['selectall']}</a><br/>
+    <select id="mod-massaction-control-section-list-select">
+    	<option value="all">{$str['allitems']}</option>
+    </select>
+    <a id="mod-massaction-control-deselectall" href="javascript:void(0);">{$str['deselectall']}</a><br/><br/>
 
-{$str['withselected']}:
+    {$str['withselected']}:
 EOB;
 
-            // print the action links
+            // Print the action links.
             $action_icons = array(
                 'moveleft'     => 't/left',
                 'moveright'    => 't/right',
                 'hide'         => 't/show',
                 'show'         => 't/hide',
                 'delete'       => 't/delete'
-                //'moveto'     => 't/move'
+                //'moveto'     => 't/move',
+                //'dupto'      => 't/duplicate'
             );
 
             foreach ($action_icons as $action => $icon_path) {
@@ -117,24 +116,27 @@ EOB;
                 $action_text = get_string('action_'.$action, 'block_massaction');
 
                 $this->content->text .= <<< EOB
-<br/>
-<a id="mod-massaction-action-{$action}" class="massaction-action" href="javascript:void(0);">
-	<img src="{$pix_path}" alt="{$action_text}" title="{$action_text}"/>&nbsp;{$action_text}
-</a>
+    <br/>
+    <a id="mod-massaction-action-{$action}" class="massaction-action" href="javascript:void(0);">
+    	<img src="{$pix_path}" alt="{$action_text}" title="{$action_text}"/>&nbsp;{$action_text}
+    </a>
 EOB;
             }
             $this->content->text .= html_writer::empty_tag('br');
             $this->content->text .= <<< EOB
-<select id="mod-massaction-control-section-list-moveto">
-	<option value="">{$str['action_movetosection']}</option>
-</select>
-
-<form id="mod-massaction-control-form" name="mod-massaction-control-form" action="{$CFG->wwwroot}/blocks/massaction/action.php" method="POST">
-	<input type="hidden" id="mod-massaction-control-request" name="request" value="">
-	<input type="hidden" id="mod-massaction-instance_id" name="instance_id" value="{$this->instance->id}">
-	<input type="hidden" id="mod-massaction-return_url" name="return_url" value="{$_SERVER['REQUEST_URI']}">
-</form>
-<div id="mod-massaction-help-icon">{$OUTPUT->help_icon('usage', 'block_massaction')}</div>
+    <select id="mod-massaction-control-section-list-moveto">
+    	<option value="">{$str['action_movetosection']}</option>
+    </select>
+    <select id="mod-massaction-control-section-list-dupto">
+    	<option value="">{$str['action_duptosection']}</option>
+    </select>
+    <form id="mod-massaction-control-form" name="mod-massaction-control-form" action="{$CFG->wwwroot}/blocks/massaction/action.php" method="POST">
+    	<input type="hidden" id="mod-massaction-control-request" name="request" value="">
+    	<input type="hidden" id="mod-massaction-instance_id" name="instance_id" value="{$this->instance->id}">
+    	<input type="hidden" id="mod-massaction-return_url" name="return_url" value="{$_SERVER['REQUEST_URI']}">
+    </form>
+    <div id="mod-massaction-help-icon">{$OUTPUT->help_icon('usage', 'block_massaction')}</div>
+</div>
 EOB;
         }
 
