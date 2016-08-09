fb7df949c0b2c39504ef16c37218aff2cd19e093
Rajesh Taneja <rajesh@moodle.com>
2016-08-09 13:12:27 +0800

    MDL-46514 behat: Delete tablesupdatedbyscenario before suite
    Delete tablesupdatedbyscenario before db reset to ensure
    db is fully reset. In case user exits or process terminated
    then the file will be left and can lead to wrong results

--- lib/testing/classes/util.php
+++ lib/testing/classes/util.php
@@ -949,9 +949,6 @@ abstract class testing_util {
         if (file_exists($tablesupdatedfile)) {
             unlink($tablesupdatedfile);
         }
-
-        // Reset static cache of cli process.
-        self::reset_updated_table_list();
     }
 
     /**
